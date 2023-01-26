720694191a51ad08f5a405fcf431edaaca3e36c0
varinmammoth <88394052+varinmammoth@users.noreply.github.com>
2023-01-26 12:59:41 +0000

    Selection criteria code working. Still need fix criterias.

--- src/selection_criteria_efficient.py
+++ src/selection_criteria_efficient.py
@@ -6,14 +6,6 @@ import pandas as pd
 def load_file(path: str):
     return pd.read_csv(path)
 
-def filter_single_prob(x: str, y:str, thres: float, df):
-    return df[df[x+'_ProbNN'+y] >= thres]
-
-def filter_prob(x_ls: list, y_ls: list, probability_thres, df):
-    for i in range(0, len(x_ls)):
-        data = filter_prob(x_ls[i], y_ls[i], probability_thres, df)
-    return data
-
 def filter_criteria(feature_ls: list, thres_ls: list, operator: list, df):
     """Filters all the columns provided in feature_ls according to the threshold
     provided in thres_ls, according to operator in operator_ls
@@ -30,7 +22,7 @@ def filter_criteria(feature_ls: list, thres_ls: list, operator: list, df):
         operator (list): List of operators.
         df (pd dataframe): The dataframe to be filterd.
     """
-    for i, feature in enumerate(feature_ls[0]):
+    for i, feature in enumerate(feature_ls[2:4]):
         if operator[i] == '<':
             df = df[df[feature] < thres_ls[i]]
         elif operator[i] == '<=':
@@ -41,38 +33,33 @@ def filter_criteria(feature_ls: list, thres_ls: list, operator: list, df):
             df = df[df[feature] >= thres_ls[i]]
         else:
             print(f'An invalid operator has been given for the feature {feature}. Not filtered this column.')
-
+    return df
 
 # %%
-def main():
-    data = load_file('Kstarp_pi0.csv')
-
-    X = ['mu_plus', 'mu_minus', 'K', 'Pi']
-    Y = ['mu', 'mu', 'k', 'pi']
-    probability_thres = 0.8
-    data_filtered = filter_prob(X, Y, probability_thres, data)
-
-    feature_ls = ['mu_plus_IPCHI2_OWNPV', 'mu_minus_IPCHI2_OWNPV', 'K_IPCHI2_OWNPV', 'Pi_IPCHI2_OWNPV'
+def filter_dataset(data):
+    feature_ls = ['mu_plus_ProbNNmu', 'mu_minus_ProbNNmu', 'K_ProbNNk', 'Pi_ProbNNpi',
+        'mu_plus_IPCHI2_OWNPV', 'mu_minus_IPCHI2_OWNPV', 'K_IPCHI2_OWNPV', 'Pi_IPCHI2_OWNPV',
         'B0_IPCHI2_OWNPV', 'B0_M', 'B0_M', 'B0_DIRA_OWNPV', 'B0_FDCHI2_OWNPV',
         'Kstar_M', 'Kstar_FDCHI2_OWNPV',
         'J_psi_M'
     ]
-    thres_ls = [
+    thres_ls = [0.8, 0.8, 0.8, 0.8,
         9, 9, 9, 9,
         16, 4850, 5780, np.cos(14e-3), 121,
         6200, 16,
         7100
     ]
-    operator_ls = [
+    operator_ls = ['>', '>', '>', '>',
         '>', '>', '>', '>',
         '<', '>', '<', '<', '>',
         '<', '>',
         '<'
     ]
-    data_filtered = filter_criteria(feature_ls, thres_ls, operator_ls)
+    data_filtered = filter_criteria(feature_ls, thres_ls, operator_ls, data)
     return data_filtered
 
-filtered_test = main()
+data = load_file('sig.csv')
+filtered_data = filter_dataset(data)
 
     
 # %%
