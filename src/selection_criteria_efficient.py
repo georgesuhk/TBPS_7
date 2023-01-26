61185db2dbd8c47d60fa6b6d6e4f5b15ce75b855
Shen Chiang <henry09027@gmail.com>
2023-01-26 13:30:17 +0000

    Update selection_criteria_efficient.py

--- src/selection_criteria_efficient.py
+++ src/selection_criteria_efficient.py
@@ -1,6 +1,8 @@
 #%%
 import numpy as np
 import pandas as pd
+import os
+from tqdm import tqdm
 # %%
 #Load file as CSV
 def load_file(path: str):
@@ -22,7 +24,8 @@ def filter_criteria(feature_ls: list, thres_ls: list, operator: list, df):
         operator (list): List of operators.
         df (pd dataframe): The dataframe to be filterd.
     """
-    for i, feature in enumerate(feature_ls[2:4]):
+    for i, feature in enumerate(feature_ls):
+        
         if operator[i] == '<':
             df = df[df[feature] < thres_ls[i]]
         elif operator[i] == '<=':
@@ -34,6 +37,42 @@ def filter_criteria(feature_ls: list, thres_ls: list, operator: list, df):
         else:
             print(f'An invalid operator has been given for the feature {feature}. Not filtered this column.')
     return df
+
+
+def examine_filters(feature_ls: list, thres_ls: list, operator: list, df):
+    """Filters all the columns provided in feature_ls according to the threshold
+    provided in thres_ls, according to operator in operator_ls
+
+    Example:
+    feature_ls = ['height', 'weight']
+    thres_ls = [180, 70]
+    operator = ['>', '<']
+    This keeps all rows with height > 180 and weight < 70.
+
+    Args:
+        feature_ls (list): List of features.
+        thres_ls (list): List of thresholds.
+        operator (list): List of operators.
+        df (pd dataframe): The dataframe to be filterd.
+    Return a dataframe with the percentage of data remaining.
+    """
+    df_len = len(df)
+    ratios = []
+    for i, feature in enumerate(feature_ls):
+        if operator[i] == '<':
+            filtered_df_len = len(df[df[feature] < thres_ls[i]])
+        elif operator[i] == '<=':
+            filtered_df_len = len(df[df[feature] <= thres_ls[i]])
+        elif operator[i] == '>':
+            filtered_df_len = len(df[df[feature] > thres_ls[i]])
+        elif operator[i] == '>=':
+            filtered_df_len = len(df[df[feature] >= thres_ls[i]])
+        else:
+            print(f'An invalid operator has been given for the feature {feature}. Not filtered this column.')
+        ratios.append(filtered_df_len/df_len)
+    return ratios
+
+
 
 # %%
 def filter_dataset(data):
@@ -55,11 +94,20 @@ def filter_dataset(data):
         '<', '>',
         '<'
     ]
-    data_filtered = filter_criteria(feature_ls, thres_ls, operator_ls, data)
-    return data_filtered
+    #data_filtered = filter_criteria(feature_ls, thres_ls, operator_ls, data)
+    #return data_filtered
+    return examine_filters(feature_ls, thres_ls, operator_ls, data)
+    
+    
 
-data = load_file('sig.csv')
-filtered_data = filter_dataset(data)
+
+def main():
+    
+    files = os.listdir('./data/')
+    list_of_ratios = [filter_dataset(load_file(file_name)) for file_name in tqdm(files)]
+    pd.DataFrame.from_dict(dict(zip(files, list_of_ratios))).to_csv('filter_statistics.csv')
+
+    
 
     
 # %%
