2e4ff068aa4975f680925a3f6fe438e74c47fee1
saximurus <119047390+saximurus@users.noreply.github.com>
2023-01-28 15:56:38 +0000

    cleaned up comments/documentation

--- fitting_functions.py
+++ fitting_functions.py
@@ -10,17 +10,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import iminuit
 
+data_path = 'C:/Users/victo/ic-teach-kstmumu-public/kstarmumu_toy_data/'
 #remember to have the correct path for this part
 #currently accessing toy data
-files = [f'toy_data_bin_{i}.csv' for i in range(7)]
+files = [f'{data_path}toy_data_bin_{i}.csv' for i in range(7)]
 bins = [pd.read_csv(file) for file in files]
 
 
 
-#Probability distributions across angles
+#Functions defined for fitting
 
 
-def projection_ctk(cos_theta_k, afb, fl, s3, s9):
+def projection_ctk(cos_theta_k, afb=0, fl=0, s3=0, s9=0):
     '''
     The projection of the pdf onto cos(theta_k) 
     
@@ -38,7 +39,7 @@ def projection_ctk(cos_theta_k, afb, fl, s3, s9):
     return normalized_scalar_array
 
 
-def projection_ctl(cos_theta_l, afb, fl, s3, s9):
+def projection_ctl(cos_theta_l, afb=0, fl=0, s3=0, s9=0):
     """
     The projection of the pdf onto cos(theta_l)
     
@@ -58,12 +59,13 @@ def projection_ctl(cos_theta_l, afb, fl, s3, s9):
     normalized_scalar_array = scalar_array * 2  # normalising scalar array to account for the non-unity acceptance function
     return normalized_scalar_array
 
-def projection_phi(phi, afb, fl, s3, s9):
+def projection_phi(phi, afb=0, fl=0, s3=0, s9=0):
     '''
-    The projection of the pdf onto cos(theta_k)
+    The projection of the pdf onto Phi
     
     Only parameters relevant to the function are listed in the docstring;
     this is for a general log-likelihood function
+    Parameters:
         phi: phi
         s3: S_3 observable
         s9: S_9 observable
@@ -76,9 +78,42 @@ def projection_phi(phi, afb, fl, s3, s9):
     return normalized_scalar_array
 
 
+    
+from scipy.special import erf
+
+def crystal_ball(x, mean, alpha, n, sigma):
+    '''
+    The 'Crystal Ball' function for the mass distribution
+    
+    Parameters:
+        x : domain of the function
+        mean : mean of the Gaussian
+        alpha : cutoff between power law and Gaussian
+        n : power law exponent
+        sd : standard deviation of the Gaussian
+    Returns: value at x
+    '''
+    
+    A = (n/np.abs(alpha))**n * np.exp(-(np.abs(alpha))**2 / 2)
+    B = (n/np.abs(alpha)) - np.abs(alpha)
+    C = (n/np.abs(alpha)) * (1/(n-1)) * np.exp(-(np.abs(alpha))**2 / 2)
+    D = np.sqrt(np.pi/2) * (1 + erf(np.abs(alpha))/np.sqrt(2))
+    N = 1/(sigma * (C + D))
+    
+    
+    bool_arr = ((x-mean)/sigma > -alpha)
+    gaussian_arr = bool_arr * N * np.exp(-((x-mean)**2)/(2*sigma**2))
+    power_law_arr = (bool_arr == False) * A * (B - (x-mean)/sigma)**(-n)
+    
+    return gaussian_arr + power_law_arr
+    
+    
+def mB_dist(x, mean, alpha1, alpha2, n1, n2, sigma1, sigma2):
+    return crystal_ball(x, mean, alpha1, n1, sigma1) + crystal_ball(x, mean, alpha2, n2, sigma2)   
+
 #log-likelihood function to be minimized
 
-def log_likelihood(pdf, afb, fl, s3, s9, _bin):
+def log_likelihood(pdf, afb=0, fl=0, s3=0, s9=0, _bin=0):
     """
     The negative log-likelihood of the probability distributions defined above
     
@@ -125,19 +160,19 @@ def minimize_logL_projection(pdf, _bin, afb=0, fl=0, s3=0, s9=0):
         
         
     elif pdf == projection_ctl:
-        min_func = lambda afb, fl:log_likelihood(pdf, afb=afb, fl=fl, s3=0, s9=0, _bin=_bin) # uses a placeholder lambda function of log-likelihood so that the minimizer only minimizes wrt correct observables 
+        min_func = lambda afb, fl:log_likelihood(pdf, afb=afb, fl=fl, _bin=_bin) # uses a placeholder lambda function of log-likelihood so that the minimizer only minimizes wrt correct observables 
         min_func.errordef = iminuit.Minuit.LIKELIHOOD
         m = iminuit.Minuit(min_func, afb=afb, fl=fl)
         m.limits=((-1, 1), (-1, 1)) # this is for the toy data from the Jupyter Notebook
 
     
     elif pdf == projection_ctk:
-        min_func = lambda fl:log_likelihood(pdf, afb=0, fl=fl, s3=0, s9=0, _bin=_bin)
+        min_func = lambda fl:log_likelihood(pdf, fl=fl, _bin=_bin)
         min_func.errordef = iminuit.Minuit.LIKELIHOOD
         m = iminuit.Minuit(min_func, fl=fl)
     
     elif pdf == projection_phi:
-        min_func = lambda s3, s9:log_likelihood(pdf, afb=0, fl=fl, s3=s3, s9=s9, _bin=_bin)
+        min_func = lambda s3, s9:log_likelihood(pdf, s3=s3, s9=s9, _bin=_bin)
         min_func.errordef = iminuit.Minuit.LIKELIHOOD
         m = iminuit.Minuit(min_func, s3=s3, s9=s9)
         #todo: find good boundaries on s3 and s9 for the values returned to make sense
