1e9f4eaaabb9efcdfaea1462e0439038df0be8ae
Fei0402 <122882406+Fei0402@users.noreply.github.com>
2023-02-02 15:39:02 +0000

    Update pull_test.py

--- fitting/pull_test.py
+++ fitting/pull_test.py
@@ -5,24 +5,20 @@ Created on Thu Feb  2 11:18:27 2023
 
 @author: fei
 """
-
 import numpy as np
-
-def pull_test(expected,observed,distance):
-    d = (observed-expected).flatten()
-    count=0
-    for i in range (0,len(d)):
-        if np.absolute(d[i])<distance:
-            count+=1
-    return print('Percentage rate of passing pull test=',count/len(d))
+import matplotlib.pyplot as plt
+def pull_test(expected,observed,observed_sd,nbins):
+    pull = ((observed-expected)/observed_sd).flatten()
+    return plt.hist(pull, bins=nbins)
     
 #%% examples
 #input data
 expected1d = np.array([50, 56, 50, 50, 50])
 observed1d = np.array([50, 50, 50, 50, 51])
-distance = 5
-pull_test(expected1d,observed1d,distance)
-
+observed1d_sd = np.array([1, 1, 2, 5, 2])
+nbins = 3
+pull_test(expected1d,observed1d,observed1d_sd,nbins)
+plt.show()
 #input data
 expected2d = np.array([[50, 50, 50, 50, 50],
                      [50, 50, 50, 50, 50],
@@ -30,5 +26,10 @@ expected2d = np.array([[50, 50, 50, 50, 50],
 observed2d = np.array([[50, 60, 40, 47, 53],
                      [50, 66, 40, 47, 53],
                      [40, 60, 40, 47, 53]])
-distance = 5
-pull_test(expected2d,observed2d,distance)
+observed2d_sd = np.array([[5, 6, 3, 4, 3],
+                     [0, 6, 4, 7, 3],
+                     [4, 6, 4, 4, 3]])
+nbins = 3
+pull_test(expected2d,observed2d,observed2d_sd,nbins)
+plt.show()
+
