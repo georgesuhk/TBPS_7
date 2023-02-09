06222964ca00c6d5bd61f1503addbb9e3e726f94
Fei0402 <122882406+Fei0402@users.noreply.github.com>
2023-02-09 21:42:02 +0000

    Update chisq_test.py

--- fitting/chisq_test.py
+++ fitting/chisq_test.py
@@ -9,7 +9,7 @@ import scipy.stats as stats
 import numpy as np
 from scipy.stats import chi2
 
-def chisq_1d(expected,observed,con_level):
+def chisq(expected,observed,rejection_factor):
     '''
     perform Chi-Square Goodness of Fit Test
         H0: (null hypothesis) A variable follows a hypothesized distribution.
@@ -17,7 +17,6 @@ def chisq_1d(expected,observed,con_level):
     
         Chi-Square test statistic, and the corresponding p-value
         p correspond to dof = (len(datapoints)-1) = n-1
-        con_level is the confidence level
     '''
     statistic, pvalue = stats.chisquare(f_obs=observed, f_exp=expected)
     if pvalue > rejection_factor:
@@ -25,38 +24,38 @@ def chisq_1d(expected,observed,con_level):
     else:
         return print('p-value=%.5f:',pvalue,' reject the null hypothesis')
 
-def chisq_ds(expected,observed,con_level):
-    '''
-    perform Chi-Square Goodness of Fit Test
-        H0: (null hypothesis) A variable follows a hypothesized distribution.
-        H1: (alternative hypothesis) A variable does not follow a hypothesized distribution.
+# def chisq_ds(expected,observed,rejection_factor):
+#     '''
+#     perform Chi-Square Goodness of Fit Test
+#         H0: (null hypothesis) A variable follows a hypothesized distribution.
+#         H1: (alternative hypothesis) A variable does not follow a hypothesized distribution.
     
-        Chi-Square test statistic, and the corresponding p-value
-        p correspond to dof = (len(datapoints)-1) = n-1
-    '''
-    statistic, pvalue = stats.chisquare(f_obs=observed, f_exp=expected)
-    chisq = np.sum(statistic)
-    rows = observed.shape[0]
-    cols = observed.shape[1]
-    df = (rows - 1) * (cols - 1)
-    p = 1 - chi2.cdf(chisq, df)
-    if p > rejection_factor:
-        print('p-value=%.5f:'%p,' fail to reject the null hypothesis')
-    else:
-        print('p-value=%.5f:'%p,' reject the null hypothesis')
+#         Chi-Square test statistic, and the corresponding p-value
+#         p correspond to dof = (len(datapoints)-1) = n-1
+#     '''
+#     statistic, pvalue = stats.chisquare(f_obs=observed, f_exp=expected)
+#     chisq = np.sum(statistic)
+#     rows = observed.shape[0]
+#     cols = observed.shape[1]
+#     df = (rows - 1) * (cols - 1)
+#     p = 1 - chi2.cdf(chisq, df)
+#     if p > rejection_factor:
+#         print('p-value=%.5f, rejection_factor=%.5f:'%(p, rejection_factor),' fail to reject the null hypothesis')
+#     else:
+#         print('p-value=%.5f, rejection_factor=%.5f:'%(p, rejection_factor),' reject the null hypothesis')
+    
 #%% examples
 #input data
-expected1d = np.array([50, 50, 50, 50, 50])
-observed1d = np.array([50, 60, 40, 47, 53])
-con_level1d = 0.95
-chisq_1d(expected1d,observed1d,con_level1d)
+import pandas as pd
+fit=pd.read_csv('toy_data_observable_values.csv')
+fit2=pd.read_csv('toy_data_observable_errors.csv')
+df=pd.read_csv('toy_data_bin_0.csv')
 
-#input data
-expected2d = np.array([[50, 50, 50, 50, 50],
-                     [50, 50, 50, 50, 50],
-                     [50, 50, 50, 50, 50]])
-observed2d = np.array([[50, 60, 40, 47, 53],
-                     [50, 66, 40, 47, 53],
-                     [40, 60, 40, 47, 53]])
-con_level2d = 0.05
-chisq_ds(expected2d,observed2d,con_level2d)
+ctk = df.ctk #toy data
+fl=fit.fl_from_ctk[0] #fitted Fl from bin 0
+ctk_fitted_error=fit2.fl_from_ctk[0] #error of fitted Fl from bin 0
+
+ctk_fitted = 3/2 * fl * ctk**2 + 3/4 * (1-fl) * (1-ctk**2)
+rejection_factor1d = 0.95
+chisq(ctk_fitted,ctk,rejection_factor1d)
+
